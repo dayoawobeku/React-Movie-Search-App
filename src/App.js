3b33eda6ce10999a84534873385cf5d5d414cded
Dayo Awobeku <dawobeku@gmail.com>
2021-06-16 17:31:03 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-8">
+        Button
+      </button>
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-light py-8 px-4 rounded">
+        What
+      </button>
+    </div>
+  );
+}
+
+export default App;
